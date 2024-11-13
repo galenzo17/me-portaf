3608ffa3db8fa28db215e0a16bfd2771afaa2162
galenzo17 <bereciartua.agustin@gmail.com>
2024-11-13 19:55:23 -0300

    update background and title

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
-import { component$ } from '@builder.io/qwik';
-import { LanguageProvider } from './context/LanguageContext';
-import { Header } from './components/Header';
-import { Experience } from './components/Experience';
-import { Skills } from './components/Skills';
-import { SocialLinks } from './components/SocialLinks';
-import { MouseFollow } from './components/MouseFollow';
-import './app.css';
+import { component$ } from "@builder.io/qwik";
+import { LanguageProvider } from "./context/LanguageContext";
+import { Header } from "./components/Header";
+import { Experience } from "./components/Experience";
+import { Skills } from "./components/Skills";
+import { SocialLinks } from "./components/SocialLinks";
+import { MouseFollow } from "./components/MouseFollow";
+import "./app.css";
 
 export const App = component$(() => {
   return (
     <LanguageProvider>
-      <div class="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white p-8 relative overflow-hidden">
+      <div class="min-h-screen w-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
         <MouseFollow />
         <div class="relative z-10">
           <Header />
