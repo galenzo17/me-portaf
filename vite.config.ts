fccf096d5e11a17a2e595fcfdb31bb0c62d63cf6
galenzo17 <bereciartua.agustin@gmail.com>
2024-11-13 19:48:50 -0300

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [qwikVite({ client: { input: "src/main.tsx" } })],
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
 });
