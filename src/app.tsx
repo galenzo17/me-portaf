432a142f251f22a862d63a561af3bba30ff17350
Agustin Bereciartua Castillo <galenzo17@users.noreply.github.com>
2025-06-01 21:52:06 -0400

    feat: add zoomable canvas component

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,23 @@ import { Experience } from "./components/Experience";
 import { Skills } from "./components/Skills";
 import { SocialLinks } from "./components/SocialLinks";
 import { MouseFollow } from "./components/MouseFollow";
+import { ZoomCanvas } from "./components/ZoomCanvas";
 import "./app.css";
 
 export const App = component$(() => {
   return (
     <LanguageProvider>
-      <div class="min-h-screen w-full bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
-        <MouseFollow />
-        <div class="relative z-10">
-          <Header />
-          <Experience />
-          <Skills />
-          <SocialLinks />
+      <ZoomCanvas>
+        <div class="min-h-screen w-full bg-gradient-to-br from-gray-900 to-gray-800 text-white relative overflow-hidden">
+          <MouseFollow />
+          <div class="relative z-10">
+            <Header />
+            <Experience />
+            <Skills />
+            <SocialLinks />
+          </div>
         </div>
-      </div>
+      </ZoomCanvas>
     </LanguageProvider>
   );
 });
